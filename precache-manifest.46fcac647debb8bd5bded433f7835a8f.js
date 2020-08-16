b04a8071bf2e1ae16d939fe85099f135b4df1338
Alonso Parra <alonsogparra@gmail.com>
2020-08-16 00:01:20 -0400

    Updates

---
+++ precache-manifest.46fcac647debb8bd5bded433f7835a8f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf331540d55f914a0850a945f8da706",
+    "url": "/new-hangman-react/index.html"
+  },
+  {
+    "revision": "b5d9bd8a7878249e8283",
+    "url": "/new-hangman-react/static/css/2.6d4ecc00.chunk.css"
+  },
+  {
+    "revision": "300a17b2062fd45ecdd8",
+    "url": "/new-hangman-react/static/css/main.16026de6.chunk.css"
+  },
+  {
+    "revision": "b5d9bd8a7878249e8283",
+    "url": "/new-hangman-react/static/js/2.980af01b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/new-hangman-react/static/js/2.980af01b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "300a17b2062fd45ecdd8",
+    "url": "/new-hangman-react/static/js/main.454ac9fd.chunk.js"
+  },
+  {
+    "revision": "59eab3ac39a491dde61e",
+    "url": "/new-hangman-react/static/js/runtime-main.bf3f6581.js"
+  },
+  {
+    "revision": "047ddb7802fb3aa2909de721925f1962",
+    "url": "/new-hangman-react/static/media/fail.047ddb78.mp3"
+  },
+  {
+    "revision": "326addd25b6b1679c0b280db4d635504",
+    "url": "/new-hangman-react/static/media/step0.326addd2.svg"
+  },
+  {
+    "revision": "77c02aeddc3bdedc262ad411fd57e7ee",
+    "url": "/new-hangman-react/static/media/step1.77c02aed.svg"
+  },
+  {
+    "revision": "d1bde5a146538e4250af9fb0602ad073",
+    "url": "/new-hangman-react/static/media/step2.d1bde5a1.svg"
+  },
+  {
+    "revision": "ce1a528ef4098072bd12d2596e2a6129",
+    "url": "/new-hangman-react/static/media/step3.ce1a528e.svg"
+  },
+  {
+    "revision": "acf26beff74466f4cb550cc058ba55b1",
+    "url": "/new-hangman-react/static/media/step4.acf26bef.svg"
+  },
+  {
+    "revision": "9e01ce1ad7b179bc0b8bc4f53b457cfb",
+    "url": "/new-hangman-react/static/media/step5.9e01ce1a.svg"
+  },
+  {
+    "revision": "5acc4c358abe356ab34cb46990213c87",
+    "url": "/new-hangman-react/static/media/victory.5acc4c35.mp3"
+  }
+]);
