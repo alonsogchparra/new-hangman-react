8c16f44af7fe83ce4e7acece0ba9c7c95caef4ec
Alonso Parra <alonsogparra@gmail.com>
2020-08-15 21:29:25 -0400

    Loading component built

---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Loading extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Cargando...</h3>
+      </div>
+    )
+  }
+}
+
+export default Loading;
